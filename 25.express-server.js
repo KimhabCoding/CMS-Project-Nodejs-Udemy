522a3717af4a9b7ed73b6d6d997109090b01e6d0
Anoskh <kimhab.zeecode@gmail.com>
2021-01-15 13:21:32 +0700

    Route nodejs video part 28

--- 25.express-server.js
+++ 25.express-server.js
@@ -1,11 +1,26 @@
 const express = require('express'),
     log = console.log,
-    PORT = 3022; 
+
+    /*  process.env.PORT || 3022
+        default on 3022 , if your run node yourfile (25.ex..js) with 'node PORT=3024 25.express-server.js'  
+        so it run on your customize port 3024 
+    */
+    PORT = process.env.PORT || 3022; 
+
 let app = express(); 
+
+app.get('/', (req, res)=> {
+    res.send('<h1>Hello Routes Nodejs</h1>');
+}); 
+
+app.get('/api', (req, res)=> {
+    // res.send('<h1>This is API');
+    res.json({ name: 'Kimhab Coding' }); 
+}); 
 
 app.listen(PORT); 
 
-log('Server is running now.')
+log('Server is running now.');
 
 // log(express.application.listen); 
 // More about express: https://www.npmjs.com/package/express | https://github.com/expressjs/express
