e25e17fdb783764ab86536ff83c688b015624acf
Anoskh <kimhab.zeecode@gmail.com>
2021-01-18 15:40:58 +0700

    Mongoose ODM VD 49

---
+++ 49.mongoose-ODM.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose'); 
+const log = console.log; 
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017/mongoose';
+
+// mongoose.connect(url, { useNewUrlParser: true }); 
+mongoose.connect(url, { useNewUrlParser: true }); 
+mongoose.connection
+    .once('open', () => log('Connected'))
+    .on('error', (err) => {
+        
+        log(`Could not connect`, err); 
+    }); 
