eeeb377eaf249613b9079b11888f87b8982ce9e2
Anoskh <kimhab.zeecode@gmail.com>
2021-01-18 16:37:57 +0700

    Create Model Mongoose Part 2 VD 51

--- 49.mongoose-ODM.js
+++ 49.mongoose-ODM.js
@@ -3,13 +3,26 @@ const User = require('./models/User');
 const log = console.log; 
  
 // Connection URL
-const url = 'mongodb://localhost:27017/mongoose';
+const url = 'mongodb://localhost:27017/myproject';
 
 // mongoose.connect(url, { useNewUrlParser: true }); 
-mongoose.connect(url, { useNewUrlParser: true }); 
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}); 
 mongoose.connection
     .once('open', () => log('Connected'))
     .on('error', (err) => {
         
         log(`Could not connect`, err); 
-    }); 
+    });  
+
+const newUser = new User({
+
+    firstName: 'Ho', 
+    lastName: 'Kimhab', 
+    isActive: 1 
+
+}); 
+
+newUser.save(function(err, dataSaved) {
+    if (err) return err; 
+    log(dataSaved); 
+}); 
--- models/User.js
+++ models/User.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose'); 
 
 // More about model: https://mongoosejs.com/docs/models.html
-const User = mongoose.model('users', {
+const schemaUser = new mongoose.Schema({
     firstName: {
-        type: String, 
-        required: true, 
-        minlength: 4, 
+        type: String,
+        required: true,
+        minlength: 4,
         trim: true
-    }, 
+    },
     lastName: {
-        type: String, 
-        required: true, 
-        minlength: 4, 
+        type: String,
+        required: true,
+        minlength: 4,
         trim: true
-    }, 
+    },
     isActive: {
-        type: Number, 
-        default: 0 
+        type: Number,
+        default: 0
     }
 
 }); 
+const User = mongoose.model('users', schemaUser); 
 
-module.exports = { User }; 
+// { User } file export it use like that too {User}
+module.exports = User; 
