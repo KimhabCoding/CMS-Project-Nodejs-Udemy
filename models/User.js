8ed863be35032dacf61b58ec8087679bc6fc666f
Anoskh <kimhab.zeecode@gmail.com>
2021-01-18 15:53:01 +0700

    Create Models in Mongoose Part1 VD 50

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose'); 
+
+// More about model: https://mongoosejs.com/docs/models.html
+const User = mongoose.model('users', {
+    firstName: {
+        type: String, 
+        required: true, 
+        minlength: 4, 
+        trim: true
+    }, 
+    lastName: {
+        type: String, 
+        required: true, 
+        minlength: 4, 
+        trim: true
+    }, 
+    isActive: {
+        type: Number, 
+        default: 0 
+    }
+
+}); 
+
+module.exports = { User }; 
