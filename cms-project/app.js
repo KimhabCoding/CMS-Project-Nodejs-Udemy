1a0b51e16e6201685691c5895ff896c63f6e5618
Anoskh <kimhab.zeecode@gmail.com>
2021-01-20 12:43:38 +0700

    Setup home page, Install express-handlebars VP 73

--- cms-project/app.js
+++ cms-project/app.js
@@ -1,11 +1,27 @@
-const express = require('express'); 
-const app = express(); 
-const log = console.log; 
+const express = require("express");
+const app = express();
+const path = require("path");
+const exphbs  = require('express-handlebars');
+const log = console.log;
 
+// Express-handlebars https://github.com/express-handlebars/express-handlebars
+// defaultLayout: https://github.com/express-handlebars/express-handlebars#defaultlayout
+// 'home' is folder 
+app.engine('handlebars', exphbs({defaultLayout: 'home'}));
+app.set('view engine', 'handlebars');
 
+// Path: https://expressjs.com/en/api.html#app.use
+// GET /style.css etc
+app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = 3024 || process.env.PORT; 
+// Get
+app.get("/", (req, res) => {
+//   res.send("Hello Method Get");
+  res.render('home/index');
+});
+
+const PORT = 3024 || process.env.PORT;
 
 app.listen(PORT, () => {
-    log(`Running on port ${PORT}`); 
-}); 
+  log(`Running on port ${PORT}`);
+});
